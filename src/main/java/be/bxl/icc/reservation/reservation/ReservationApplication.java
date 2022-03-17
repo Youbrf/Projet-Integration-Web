b574d4be3dca093c0426a6da72699e93c3ac8da7
Youssef Ch <ychennou@gmail.com>
2022-03-17 20:34:53 +0100

    base

---
+++ src/main/java/be/bxl/icc/reservation/reservation/ReservationApplication.java
@@ -0,0 +1,13 @@
+package be.bxl.icc.reservation.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/bxl/icc/reservation/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bxl.icc.reservation.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
