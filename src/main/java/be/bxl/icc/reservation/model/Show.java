ed55536c7f2b437632fb29374163daa8f940bbab
Youssef Chennou <ychennou@gmail.com>
2022-05-15 16:10:55 +0200

    Modèle Representation +Relation ManyToOne with Location & Show

--- src/main/java/be/bxl/icc/reservation/model/Show.java
+++ src/main/java/be/bxl/icc/reservation/model/Show.java
@@ -1,6 +1,8 @@
 package be.bxl.icc.reservation.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Column;
 
@@ -50,6 +53,33 @@ public class Show {
 	 */
 	@Column(name="updated_at")
 	private LocalDateTime updatedAt;
+
+	@OneToMany(targetEntity=Representation.class, mappedBy="show")
+	private List<Representation> representations = new ArrayList<>();
+
+	public List<Representation> getRepresentations() {
+		return representations;
+	}
+
+	public Show addRepresentation(Representation representation) {
+		if(!this.representations.contains(representation)) {
+			this.representations.add(representation);
+			representation.setShow(this);
+		}
+		
+		return this;
+	}
+	
+	public Show removeRepresentation(Representation representation) {
+		if(this.representations.contains(representation)) {
+			this.representations.remove(representation);
+			if(representation.getLocation().equals(this)) {
+				representation.setLocation(null);
+			}
+		}
+		
+		return this;
+	}
 
 	public Show() { }
 	
@@ -152,7 +182,9 @@ public class Show {
 		return "Show [id=" + id + ", slug=" + slug + ", title=" + title 
 			+ ", description=" + description + ", posterUrl=" + posterUrl + ", location=" 
 			+ location + ", bookable=" + bookable + ", price=" + price
-			+ ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
+			+ ", createdAt=" + createdAt + ", updatedAt=" + updatedAt 
+			+ ", representations=" + representations.size() + "]";
 	}
+
 	
 }
