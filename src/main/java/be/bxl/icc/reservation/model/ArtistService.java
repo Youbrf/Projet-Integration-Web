c7157af6537b517182225d6af80df4e914770d67
Youssef Chennou <ychennou@gmail.com>
2022-05-17 23:00:03 +0200

    Fin de la partie mise à jour

--- src/main/java/be/bxl/icc/reservation/model/ArtistService.java
+++ src/main/java/be/bxl/icc/reservation/model/ArtistService.java
@@ -33,8 +33,8 @@ public class ArtistService {
 		artistRepository.save(artist);
 	}
 
-	public void deleteArtist(String id) {
-		Long indice = (long) Integer.parseInt(id);
+	public void deleteArtist(Long indice2) {
+		Long indice = indice2;
 		
 		artistRepository.deleteById(indice);
 	}
