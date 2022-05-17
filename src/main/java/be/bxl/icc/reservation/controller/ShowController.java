4223181f1f9534f3280ad9bf89fe5f1ebb4169f1
Youssef Chennou <ychennou@gmail.com>
2022-05-17 18:26:51 +0200

    Update templates Show (added artists’ list) ERROR JAVACLASSCASTEXCEPTION

--- src/main/java/be/bxl/icc/reservation/controller/ShowController.java
+++ src/main/java/be/bxl/icc/reservation/controller/ShowController.java
@@ -1,6 +1,10 @@
 package be.bxl.icc.reservation.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import be.bxl.icc.reservation.model.ArtistType;
 import be.bxl.icc.reservation.model.Show;
 import be.bxl.icc.reservation.model.ShowService;
+import be.bxl.icc.reservation.model.Type;
+import javassist.expr.Instanceof;
 
 
 @Controller
@@ -30,6 +37,19 @@ public class ShowController {
 	@GetMapping("/shows/{id}")
    	 public String show(Model model, @PathVariable("id") String id) {
 		Show show = service.get(id);
+
+		//Récupérer les artistes du spectacle et les grouper par type
+        Map<Type,ArrayList<ArtistType>> collaborateurs = new TreeMap<>();
+        
+//        show.getArtistTypes().forEach((a)->{
+//        	if(a.getType() == null) {
+//            	collaborateurs.put(a.getType(), new ArrayList<>());
+//            }
+//            collaborateurs.get(a.getType()).add(a);
+//        });
+//       
+        
+        model.addAttribute("collaborateurs", collaborateurs);
 
 		model.addAttribute("show", show);
 		model.addAttribute("title", "Fiche d'un spectacle");
--- src/main/java/be/bxl/icc/reservation/model/TypeRepository.java
+++ src/main/java/be/bxl/icc/reservation/model/TypeRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface TypeRepository extends CrudRepository<Type, Long> {
 	Type findByType(String type);
 	Optional<Type> findById(Long id);
+	@Override Iterable<Type> findAll();
 
 }
