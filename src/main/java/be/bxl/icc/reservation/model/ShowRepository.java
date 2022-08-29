7d82b43fedce996fb476ee53ec9fcc144774aab6
Youssef Chennou <ychennou@gmail.com>
2022-08-29 19:16:28 +0200

    SearchShow

--- src/main/java/be/bxl/icc/reservation/model/ShowRepository.java
+++ src/main/java/be/bxl/icc/reservation/model/ShowRepository.java
@@ -3,11 +3,15 @@ package be.bxl.icc.reservation.model;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ShowRepository extends JpaRepository <Show,Long> {
 	Show findBySlug(String slug);
 	Show findByTitle(String title);
 	List<Show> findByLocation(Location location);
+
+	@Query("SELECT s FROM Show s WHERE s.title LIKE %?1%")
+	List<Show> search(String keyword);
 }
 
